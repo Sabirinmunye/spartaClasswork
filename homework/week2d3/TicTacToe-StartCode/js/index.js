3fb9e592b90fa24b258e192e4eccd7a7e06a29a4
Sabirin Munye <sabirinmunye@gmail.com>
2017-08-10 09:47:34 +0100

    Tictactoehomeworkupdate

--- homework/week2d3/TicTacToe-StartCode/js/index.js
+++ homework/week2d3/TicTacToe-StartCode/js/index.js
@@ -1,20 +1,56 @@
 $(function(event){
-	
-	console.log('dom is ready');
-})
-//variables for players and moves
 
 //player 1 move
-// var player1Move = [];
+player1 = ("");
+player2 = ("");
+var player1Move = [];
 // //player 2 move
-// var player2Move = [];
-// //winning combinations array
-// // var wins [[0,1,2],[3,4,5],[6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8],[2,4,6]],
+var player2Move = [];
+var turn = 0;
+var playerTurn = $(".playerTurn")[0];
+
+	
 
 
 // // find box attributes
-var boxes = $("td");
+	var boxes = $("td");
 
 $(boxes).each(function(index, box){
   console.log($(box).attr("data-num"));
+
+  var element = $(box);
+  // element.html('X')
+
+ //  var $box = $(box);
+  element.on('click', function(event) {
+  	console.log('being clicked');
+  	var box = $(this);
+  	if ((turn % 2) == 0){
+  		box.html('O');
+  		box.addClass('O');
+  		$(playerTurn).html("Its X's Turn");
+  		turn ++;
+
+  	}
+  	//make the buttons change when u click//
+	else{
+		var box = $(this);
+		box.html('X');
+		box.addClass('X');
+		$(playerTurn).html("Its O's Turn");
+		turn++;
+	}
+
+	
+	})
+
+})
+
+
+	// //winning combinations array
+ // var wins [[0,1,2],[3,4,5],[6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8],[2,4,6]],
+
+//variables for players and moves
+
+
 })
