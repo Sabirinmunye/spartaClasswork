619351ee9fc346087882397952816ef1c311795b
Sabirin Munye <sabirinmunye@gmail.com>
2017-08-30 09:12:52 +0100

    finished

---
+++ homework/week5d1/videocontroller/db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+Video.create([
+	{name: 'Joe', url: "https://www.youtube.com/embed/6ar-7XfCUMo", description:'if i was your man'},
+	{name: 'Joe',  url:"https://www.youtube.com/embed/YT9bUfWiC7w", description: 'dont wanna be a player'},
+	{name: 'Joe',  url: "https://www.youtube.com/embed/HUc7MWJjN5k", description: 'good girls'}
+])
